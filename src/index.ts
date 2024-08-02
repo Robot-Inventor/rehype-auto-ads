c992d5144fd47a943a3a61d5bf3ffbf75a345221
Robot-Inventor <30.keita@gmail.com>
2024-08-03 03:06:11 +0900

    fix ESLint errors

--- src/index.ts
+++ src/index.ts
@@ -108,8 +108,7 @@ const rehypeAutoAds: Plugin<[RehypeAutoAdsOptions], Root> = (args: RehypeAutoAds
             const nextNode = parent.children[index + 1];
 
             const shouldInsertAd =
-                paragraphCount >= options.paragraphInterval &&
-                options.shouldInsertAd(vfile, node, nextNode || null, ancestors);
+                paragraphCount >= options.paragraphInterval && options.shouldInsertAd(vfile, node, nextNode, ancestors);
 
             if (shouldInsertAd) {
                 // eslint-disable-next-line no-magic-numbers
