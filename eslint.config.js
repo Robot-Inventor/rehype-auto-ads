dea7b463f0b7c2469a9b3e02956ec78037d5b9d0
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2026-09-15 04:31:35 +0900

    chore: migrate from ESLint to Oxlint (#141)

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "eslint/config";
-import { eslintConfig } from "@robot-inventor/eslint-config";
-
-export default defineConfig(
-    {
-        ignores: ["**/*.test.ts"]
-    },
-    ...eslintConfig
-);
